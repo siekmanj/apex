e3b0cdadf38c98713eb50a9eaa45a117f9d45062
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-11-19 14:55:41 -0800

    added improved sampling

--- algos/ppo.py
+++ algos/ppo.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 from torch.distributions import kl_divergence
+from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 import numpy as np
 
@@ -29,6 +30,7 @@ class Buffer:
     self.size = 0
 
     self.traj_idx = [0]
+    self.buffer_ready = False
 
   def push(self, state, action, reward, value, done=False):
     self.states  += [state]
@@ -64,24 +66,28 @@ class Buffer:
     a = self.returns - self.values
     a = (a - a.mean()) / (a.std() + 1e-4)
     self.advantages = a
-    self.advantage_calculated = True
+    self.buffer_ready = True
 
-  def sample(self, batch_size=128, recurrent=False):
+  def sample(self, batch_size=64, recurrent=False):
     if not self.buffer_ready:
       self._finish_buffer()
 
     if recurrent:
       raise NotImplementedError
     else:
-      idxs = np.random.randint(0, self.size-1, size=batch_size)
-      #print(np.max(idxs), len(self.returns), len(self.states))
+      print("Creating new random indices!")
+      random_indices = SubsetRandomSampler(range(self.size))
+      sampler = BatchSampler(random_indices, batch_size, drop_last=True)
 
-      states     = self.states[idxs]
-      actions    = self.actions[idxs] 
-      returns    = self.returns[idxs]
-      advantages = self.advantages[idxs]
+      for i, idxs in enumerate(sampler):
+        print("Yielding batch {} of {}".format(i, len(sampler)))
 
-      return states, actions, returns, advantages
+        states     = self.states[idxs]
+        actions    = self.actions[idxs] 
+        returns    = self.returns[idxs]
+        advantages = self.advantages[idxs]
+
+        yield states, actions, returns, advantages
       
 class PPO:
   def __init__(self, actor, critic, args, buff, env_fn):
@@ -126,7 +132,7 @@ class PPO:
         value = self.critic(state)
         self.buffer.end_trajectory(terminal_value=(not done) * value.numpy())
 
-      return [self.buffer.sample(batch_size=64, recurrent=self.recurrent) for _ in range(batches)], num_steps
+      return num_steps
 
   def update_policy(self, epochs, batch_size):
     kl     = []
@@ -136,53 +142,55 @@ class PPO:
     ent    = []
     adv    = []
 
+    timesteps = self.collect_experience(2000, epochs)
     self.old_actor.load_state_dict(self.actor.state_dict())  # WAY faster than deepcopy
 
-    batches, timesteps = self.collect_experience(2000, epochs)
-    for i, batch in enumerate(batches):
-      states, actions, returns, advantages = batch
+    for epoch in range(epochs):
+      for i, batch in enumerate(self.buffer.sample(batch_size)):
+        states, actions, returns, advantages = batch
 
-      with torch.no_grad():
-        states = self.actor.normalize_state(states, update=False)
-        old_pdf       = self.old_actor.pdf(states)
-        old_log_probs = old_pdf.log_prob(actions).sum(-1, keepdim=True)
+        with torch.no_grad():
+          states = self.actor.normalize_state(states, update=False)
+          old_pdf       = self.old_actor.pdf(states)
+          old_log_probs = old_pdf.log_prob(actions).sum(-1, keepdim=True)
 
-      pdf       = self.actor.pdf(states)
-      log_probs = pdf.log_prob(actions).sum(-1, keepdim=True)
+        pdf       = self.actor.pdf(states)
+        log_probs = pdf.log_prob(actions).sum(-1, keepdim=True)
 
-      ratio = (log_probs - old_log_probs).exp()
+        ratio = (log_probs - old_log_probs).exp()
 
-      cpi_loss = ratio * advantages
-      clip_loss = ratio.clamp(0.8, 1.2) * advantages
-      actor_loss = -torch.min(cpi_loss, clip_loss).mean()
+        cpi_loss = ratio * advantages
+        clip_loss = ratio.clamp(0.8, 1.2) * advantages
+        actor_loss = -torch.min(cpi_loss, clip_loss).mean()
 
-      values = self.critic(states)
-      critic_loss = F.mse_loss(values, returns)
+        values = self.critic(states)
+        critic_loss = F.mse_loss(values, returns)
 
-      entropy_penalty = -self.entropy_coeff * pdf.entropy().mean()
+        entropy_penalty = -self.entropy_coeff * pdf.entropy().mean()
 
-      self.actor_optim.zero_grad()
-      (actor_loss + entropy_penalty).backward()
-      torch.nn.utils.clip_grad_norm_(self.actor.parameters(), self.grad_clip)
-      self.actor_optim.step()
+        self.actor_optim.zero_grad()
+        (actor_loss + entropy_penalty).backward()
+        torch.nn.utils.clip_grad_norm_(self.actor.parameters(), self.grad_clip)
+        self.actor_optim.step()
 
-      self.critic_optim.zero_grad()
-      critic_loss.backward()
-      torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.grad_clip)
-      self.critic_optim.step()
+        self.critic_optim.zero_grad()
+        critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.grad_clip)
+        self.critic_optim.step()
 
-      with torch.no_grad():
-        kl_div = kl_divergence(pdf, old_pdf).mean() 
+        with torch.no_grad():
 
-        kl     += [kl_div.numpy()]
-        a_loss += [actor_loss.item()]
-        c_loss += [critic_loss.item()]
-        ratios += [ratio.mean().numpy()]
-        ent    += [pdf.entropy().mean().numpy()]
-        adv    += [advantages.mean().numpy()]
-        if kl_div > 0.02:
-          print("Max KL reached, aborting update")
-          break
+          a_loss += [actor_loss.item()]
+          c_loss += [critic_loss.item()]
+          ratios += [ratio.mean().numpy()]
+          ent    += [pdf.entropy().mean().numpy()]
+          adv    += [advantages.mean().numpy()]
+
+      kl_div = kl_divergence(pdf, old_pdf).mean() 
+      kl     += [kl_div.detach().numpy()]
+      if kl_div > 0.02:
+        print("Max KL reached, aborting update")
+        break
 
     self.buffer.clear()
     return timesteps, np.mean(kl), np.mean(a_loss), np.mean(c_loss), np.mean(ratios), np.mean(ent), np.mean(adv)
@@ -223,8 +231,7 @@ def run_experiment(args):
   algo = PPO(actor, critic, args, Buffer(discount=args.discount), env_fn)
 
   steps = 0
-  prenormalize_steps = 10000
-  while steps < prenormalize_steps: # Prenormalize
+  while steps < args.prenormalize_steps: # Prenormalize
     state = torch.Tensor(eval_env.reset())
     done = False
     traj_len = 0
@@ -235,7 +242,7 @@ def run_experiment(args):
       state = torch.Tensor(next_state)
       traj_len += 1
       steps += 1
-    print("Prenormalizing {:5d}/{:5d}".format(steps, prenormalize_steps), end="\r")
+    print("Prenormalizing {:5d}/{:5d}".format(steps, args.prenormalize_steps), end="\r")
   print()
 
   # create a tensorboard logging object
@@ -246,6 +253,7 @@ def run_experiment(args):
 
 
   i = 0
+  steps = 0
   while steps < args.timesteps:
     timesteps, kl, a_loss, c_loss, ratio, entropy, adv = algo.update_policy(args.epochs, args.batch_size)
     steps += timesteps
